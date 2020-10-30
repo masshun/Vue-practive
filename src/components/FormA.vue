592ecb8ee91d07499af5950e8969061cb49cc69b
松下俊 <shunmatsu753@gmail.com>
2020-10-31 08:46:55 +0900

    mixinと動的コンポーネントの練習

---
+++ src/components/FormA.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>FormA</h1>
+
+    <input type="text" v-model="name" placeholder="input your name" />
+
+    <input type="text" v-model="msg" placeholder="input message" />
+
+    Name:{{ name }}
+
+    Message:{{ msg }}
+    <button v-on:click="check">Check</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormA',
+  data() {
+    return {
+      name: '',
+      msg: '',
+    };
+  },
+  methods: {
+    check() {
+      alert('Your name is ' + this.name);
+    },
+  },
+};
+</script>
--- src/pages/CustomContent.vue
+++ src/pages/CustomContent.vue
@@ -1,17 +1,45 @@
 <template>
-  <p>{{ subTitle | lowerCase }}</p>
+  <v-container justifiy-content-center>
+    <p>{{ subTitle | lowerCase }}</p>
+
+    <div class="flex-center">
+      <v-radio-group row v-model="selectedComponent">
+        <v-radio type="radio" id="FormA" value="FormA" label="FormA" />
+        <v-radio type="radio" id="FormB" value="FormB" label="FormB" />
+      </v-radio-group>
+    </div>
+    <!-- 動的にコンポーネントを切り替えても値は保持されるss -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </v-container>
 </template>
 
 <script>
 import commonMixin from '../helper/commonMixin';
+import FormA from '../components/FormA.vue';
+import FormB from '../components/FormB.vue';
 
 export default {
   name: 'CustomContent',
   mixins: [commonMixin],
+  components: {
+    FormA,
+    FormB,
+  },
   data() {
     return {
       subTitle: 'Learning UX design',
+      selectedComponent: '',
     };
   },
 };
 </script>
+
+<style scoped>
+.flex-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
